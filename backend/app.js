045c02a2301168052cb5ea8ce5d80ef12900d122
Hoowerr <109082395+Hoowerr@users.noreply.github.com>
2023-05-28 04:05:23 +0300

    added cors

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,15 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const validationErrors = require('celebrate').errors;
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cors = require('cors');
 const router = require('./routes/index');
 const errors = require('./middlewares/errors');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
-
+app.use(cors());
 mongoose
   .connect('mongodb://127.0.0.1:27017/mestodb')
   .then(() => {
